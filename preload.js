fbf96ddb4ee3df5d8819c3f179a9e8ac48d131dc
Luaxeviation <jaynjonjo1090@gmail.com>
2026-05-02 12:29:14 -0700

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("luax", {
-  askAssistant: (payload) => ipcRenderer.invoke("assistant:ask", payload),
-  getMeta: () => ipcRenderer.invoke("app:meta")
-});
